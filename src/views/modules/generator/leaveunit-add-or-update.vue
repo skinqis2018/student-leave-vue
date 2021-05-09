75412b13a79dc96b1877950f098fac05d8f86c98
李翊豪 <yihao6@staff.weibo.com>
2021-05-09 16:46:52 +0800

    增加班级\院系

---
+++ src/views/modules/generator/leaveunit-add-or-update.vue
@@ -0,0 +1,90 @@
+<template>
+  <el-dialog
+    :title="!dataForm.id ? '新增' : '修改'"
+    :close-on-click-modal="false"
+    :visible.sync="visible">
+    <el-form :model="dataForm" :rules="dataRule" ref="dataForm" @keyup.enter.native="dataFormSubmit()" label-width="80px">
+    <el-form-item label="院系名" prop="unitName">
+      <el-input v-model="dataForm.unitName" placeholder="院系名"></el-input>
+    </el-form-item>
+    </el-form>
+    <span slot="footer" class="dialog-footer">
+      <el-button @click="visible = false">取消</el-button>
+      <el-button type="primary" @click="dataFormSubmit()">确定</el-button>
+    </span>
+  </el-dialog>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        visible: false,
+        dataForm: {
+          unitId: 0,
+          unitName: ''
+        },
+        dataRule: {
+          unitName: [
+            { required: true, message: '院系名不能为空', trigger: 'blur' }
+          ],
+          createUserId: [
+            { required: true, message: '创建者ID不能为空', trigger: 'blur' }
+          ],
+          createTime: [
+            { required: true, message: '创建时间不能为空', trigger: 'blur' }
+          ]
+        }
+      }
+    },
+    methods: {
+      init (id) {
+        this.dataForm.unitId = id || 0
+        this.visible = true
+        this.$nextTick(() => {
+          this.$refs['dataForm'].resetFields()
+          if (this.dataForm.unitId) {
+            this.$http({
+              url: this.$http.adornUrl(`/generator/leaveunit/info/${this.dataForm.unitId}`),
+              method: 'get',
+              params: this.$http.adornParams()
+            }).then(({data}) => {
+              if (data && data.code === 0) {
+                this.dataForm.unitName = data.leaveUnit.unitName
+              }
+            })
+          }
+        })
+      },
+      // 表单提交
+      dataFormSubmit () {
+        this.$refs['dataForm'].validate((valid) => {
+          if (valid) {
+            this.$http({
+              url: this.$http.adornUrl(`/generator/leaveunit/${!this.dataForm.unitId ? 'save' : 'update'}`),
+              method: 'post',
+              data: this.$http.adornData({
+                'unitId': this.dataForm.unitId || undefined,
+                'unitName': this.dataForm.unitName
+              })
+            }).then(({data}) => {
+              if (data && data.code === 0) {
+                this.$message({
+                  message: '操作成功',
+                  type: 'success',
+                  duration: 1500,
+                  onClose: () => {
+                    this.visible = false
+                    this.$emit('refreshDataList')
+                  }
+                })
+              } else {
+                this.$message.error(data.msg)
+              }
+            })
+          }
+        })
+      }
+    }
+  }
+</script>
